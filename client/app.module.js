b2dffaefd0853754ea0e09f21b706fad8b993384
aleemnazer <aleem.nazer@gmail.com>
2018-12-17 16:05:09 +0500

    home and project tabs added

--- client/app.module.js
+++ client/app.module.js
@@ -31,8 +31,24 @@ angular.module('projecto', [
     name: 'home',
     url: '/',
     cache: false,
-    template: '<h1> welcome user </h1>',
+    templateUrl: 'home.template.html',
     resolve: { authenticate: authenticate }
+  }
+  var tab1State = {
+    name: 'home.tab1',
+    url: '/tab1',
+    template: '<p> Comments... </p>',
+  }
+  var tab2State = {
+    name: 'home.tab2',
+    url: '/tab2',
+    template: '<p> History... </p>',
+  }
+
+  var tab3State = {
+    name: 'home.tab3',
+    url: '/tab3',
+    template: '<p> Followers... </p>'
   }
 
   var usersState = {
@@ -49,6 +65,24 @@ angular.module('projecto', [
     resolve: { authenticate: authenticate }
   }
 
+  var projectTab1State = {
+    name: 'projects.tab1',
+    url: '/comments',
+    template: '<p> Project Comments... </p>',
+  }
+
+  var projectTab2State = {
+    name: 'projects.tab2',
+    url: '/history',
+    template: '<p> Project History... </p>',
+  }
+
+  var projectTab3State = {
+    name: 'projects.tab3',
+    url: '/followers',
+    template: '<p> Project Follower... </p>',
+  }
+
   var newprojectsState = {
     name: 'newproject',
     url: '/newproject',
@@ -60,11 +94,17 @@ angular.module('projecto', [
   $stateProvider.state(loginState);
   $stateProvider.state(signUpState);
   $stateProvider.state(homeState);
+  $stateProvider.state(tab1State);
+  $stateProvider.state(tab2State);
+  $stateProvider.state(tab3State);
   $stateProvider.state(usersState);
   $stateProvider.state(projectsState);
+  $stateProvider.state(projectTab1State);
+  $stateProvider.state(projectTab2State);
+  $stateProvider.state(projectTab3State);
   $stateProvider.state(newprojectsState);
   $stateProvider.state(logoutState);
-  // $locationProvider.html5Mode({enabled: true, requireBase: false});
+  $locationProvider.html5Mode({enabled: true, requireBase: false});
 
   function authenticate($q, Auth, $state, $timeout) {
     if (Auth.isLoggedIn()) {
